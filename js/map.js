cc82a82e9f522f55685ef2a755d74ef741fffb53
yarlyk <yarlyk@mail.ru>
2025-02-23 04:03:38 +0700

    Создал модуль карты,  В нём добавил слой с картой центра Токио. создал свой маркер, добавил слушатель перемещения маркера с передачей координат в поле Адрес формы

---
+++ js/map.js
@@ -0,0 +1,42 @@
+const addressMarker = document.querySelector('#address');
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    // console.log('Карта инициализирована');
+  })
+  .setView({
+    lat: 35.6854195988901,
+    lng: 139.7527348995209
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const marker = L.marker(
+  {
+    lat: 35.685434,
+    lng:139.752735
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon
+  },
+);
+
+marker.addTo(map);
+
+marker.on('moveend', (evt) => {
+  const geoMarker = evt.target.getLatLng();
+  addressMarker.value = `${geoMarker.lat} ${geoMarker.lng}`;
+});
