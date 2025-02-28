fc1132cb4dc5518fb1dcfd4218a7e736fc1845b1
yarlyk <yarlyk@mail.ru>
2025-02-28 21:31:45 +0700

    Добавил описание функций, вынес литерал в отдельную константу и передал её в перечисление

--- js/get-send-data.js
+++ js/get-send-data.js
@@ -1,16 +1,21 @@
-const dataUrl = {
-  GET_DATA_URL: 'https://28.javascript.htmlacademy.pro/keksobooking/data',
-  SEND_DATA_URL: 'https://28.javascript.htmlacademy.pro/keksobooking/'
+const url = 'https://28.javascript.htmlacademy.pro/keksobooking';
+const DataUrl = {
+  GET_DATA_URL: `${url}/data`,
+  SEND_DATA_URL: `${url}/`
 };
 
-// Функция для отображения ошибки
+
+/**
+ * Функция для отображения ошибки
+ * @param { String } message - текст ошибки
+ */
 const showError = (message) => {
   const errorTemplate = document.querySelector('#error').content.cloneNode(true);
   const errorElement = errorTemplate.querySelector('.error');
   const errorMessage = errorElement.querySelector('.error__message');
   const errorButton = errorElement.querySelector('.error__button');
 
-  // Устанавливаем сообщение об ошибк
+  // Устанавливаем сообщение об ошибке
   errorMessage.textContent = message;
 
   // Добавляем обработчик для кнопки "Попробовать снова"
@@ -23,7 +28,12 @@ const showError = (message) => {
   document.body.appendChild(errorElement);
 };
 
-const createLoader = () => fetch(dataUrl.GET_DATA_URL)
+
+/**
+ * Функция для получения данных с сервера
+ * @returns - возвращает промис, который разрешается либо в массив данных, полученных от сервера, либо в пустой массив, если произошла ошибка.
+ */
+const createLoader = () => fetch(DataUrl.GET_DATA_URL)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`Ошибка загрузки данных. Код ошибки: ${response.status}`);
@@ -32,7 +42,7 @@ const createLoader = () => fetch(dataUrl.GET_DATA_URL)
   })
   .catch((error) => {
     showError(`${error.message}`);
-    return []; // Возвращаем пустой массив, чтобы не сломать код
+    return [];
   });
 
 export { createLoader };
