d4cf94900e88a99d88df3b3eec90367ba5718733
yarlyk <yarlyk@mail.ru>
2025-12-12 15:56:27 +0400

    Создал и экспортировал переменную showApartments. В ней запрашиваются данные и обрабатываются ошибки. Вынес функцию создания сообщения об ошибке в отдельный модуль

--- js/get-send-data.js
+++ js/get-send-data.js
@@ -1,9 +1,16 @@
 import {enableForm, enableFilter} from './control-form.js';
+import { initMap } from './map.js';
+import { showError } from './maker-massage-success-error.js';
 
 const url = 'https://28.javascript.htmlacademy.pro/keksobooking';
 const DataUrl = {
   GET_DATA_URL: `${url}/data`,
   SEND_DATA_URL: `${url}/`
+};
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
 };
 
 // Перечисление текста ошибки
@@ -12,97 +19,37 @@ const TextErrorMessage = {
   SEND_DATA_ER: 'Не удаётся отправить данные'
 };
 
-// Задаем время показа сообщения об ошибке
-const timeShowErrorMessage = 7000;
-
-/**
- * Функция для отображения ошибки с отсчетом времени
- * @param {String} message - текст ошибки
- */
-const showError = (message) => {
-  const errorTemplate = document.querySelector('#error').content.cloneNode(true);
-  const errorElement = errorTemplate.querySelector('.error');
-  const errorMessage = errorElement.querySelector('.error__message'); // Для отображения поля сообщения
-  const errorButton = errorElement.querySelector('.error__button'); // Для отображения кнопки
-  const timeElement = document.createElement('div'); // Для отображения отсчета
-
-  // Добавляем стили для элемента времени
-  timeElement.style.cssText = `
-    margin-top: 50px;
-    font-size: 50 px;
-    color: #d8dbd99c;
-  `;
-
-  // Устанавливаем сообщение об ошибке
-  errorMessage.textContent = message;
-
-  // Добавляем элемент для обратного отсчета
-  errorMessage.appendChild(timeElement);
-
-  // Объявляю переменную для обратного отсчёта
-  let timeLeft = timeShowErrorMessage / 1000;
-
-  /**
- * Функция для обновления текста обратного отсчёта
-  * @param {Number} timeLeft - время в миллисекундах, через которое сообщение самоуничтожится
- */
-  const updateCountdown = () => {
-    timeElement.textContent = `Агент Хант, это сообщение самоуничтожится через ${timeLeft} сек.`;
-  };
-
-  updateCountdown();
-
-  // Запускаем интервал для обратного отсчета
-  const countdownInterval = setInterval(() => {
-    timeLeft--;
-    updateCountdown();
-
-    // Останавливаем бесконечный setInterval при достижении 0
-    if (timeLeft <= 0) {
-      clearInterval(countdownInterval);
-    }
-  }, 1000);
-
-  // Добавляем обработчик для кнопки "Попробовать снова"
-  errorButton.addEventListener('click', () => {
-    clearInterval(countdownInterval);
-    errorElement.remove();
-    window.location.reload(); // Перезагружаем страницу
-  });
-
-  // Добавляем ошибку в DOM
-  document.body.appendChild(errorElement);
-
-  // Добавляем самоуничтожение сообщения об ошибке через установленный интервал времени и
-  // снимаем блокировку с фильтра и формы отправки объявления
-  const autoRemoveTimeout = setTimeout(() => {
-    errorElement.remove();
-    enableForm();
-    enableFilter();
-  }, timeShowErrorMessage);
-
-  // Очистка таймеров при удалении сообщения об ошибке
-  errorElement.addEventListener('remove', () => {
-    clearInterval(countdownInterval);
-    clearTimeout(autoRemoveTimeout);
-  });
-};
-
 /**
  * Функция для получения данных с сервера
  * @returns - возвращает промис, который разрешается либо в массив данных, полученных от сервера, либо в пустой массив, если произошла ошибка.
  */
-const createLoader = () => fetch(DataUrl.GET_DATA_URL)
+const loader = (route, method = Method.GET, body = null) => fetch(route, {method, body})
   .then((response) => {
     if (!response.ok) {
-      throw new Error('Ошибка загрузки данных. Что-то данные не подгружаются');
+      throw new Error();
     }
     return response.json();
   })
-  .catch((error) => {
-    // Используем перехваченную ошибку для показа сообщения и возвращаем пустой массив
-    showError(`${TextErrorMessage.GET_DATA_ER}`);
-    return [];
+  .catch(() => {
+    enableForm();
+    enableFilter();
+    throw new Error();
   });
 
-export { createLoader };
+const getData = () => loader(DataUrl.GET_DATA_URL);
+
+// Загружаем данные с сервера и инициализируем карту
+const showApartments = getData()
+  .then((apartments) => {
+    if (apartments.length > 0) {
+      initMap(apartments);
+    }
+  })
+  .catch(() => {
+      showError(TextErrorMessage.GET_DATA_ER);
+  });
+
+const sendAd = () => loader(DataUrl.SEND_DATA_URL, Method.POST, body);
+
+
+export { showApartments, sendAd };
