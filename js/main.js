ebeb848eeedd9c6c9fb484a99f597794088271b4
yarlyk <yarlyk@mail.ru>
2025-12-12 15:59:12 +0400

    Убрал импорт ненужных функций initMap, getData. Минимизировал код в точке входа.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 import { disableForm, disableFilter } from './control-form.js';
-import { initMap } from './map.js';
 import { createUiSlider } from './no-ui-slider.js';
 import { validatingFormSubmit } from './validate-form.js';
-import { createLoader } from './get-send-data.js';
+import { showApartments } from './get-send-data.js';
 
 // Осуществляем вызовы функций блокировки формы подачи объявления и фильтра до загрузки карты
 disableForm();
@@ -15,9 +14,4 @@ createUiSlider();
 validatingFormSubmit();
 
 // Загружаем данные с сервера и инициализируем карту
-createLoader()
-  .then((apartments) => {
-    if (apartments.length > 0) {
-      initMap(apartments);
-    }
-  });
+showApartments();
